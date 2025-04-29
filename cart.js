923a92eff54a55e5d04b980c47fadd3ab8ef4e56
Gchandrakala1987 <167696567+Gchandrakala1987@users.noreply.github.com>
2025-04-29 22:50:31 +0000

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,7 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push({ name, price });
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert('Product added to cart!');
+  }
+  
